2f1ae61b97b7731e1634394f9f671a48d6a86cf2
zibersaioros <zibersaioros@gmail.com>
2015-10-01 00:52:36 +0900

    1. 컨트롤러 작성중

---
+++ src/main/java/com/rs/lottoweb/domain/Variable.java
@@ -0,0 +1,18 @@
+package com.rs.lottoweb.domain;
+
+public interface Variable {
+
+	public static final String EX_ANAL_COUNT = "exclusionAnalysisCount";
+	public static final String EX_MIN_RANGE = "exclusionMinRange";
+	public static final String EX_MAX_RANGE = "exclusionMaxRange";
+	public static final String EX_RANGE_INC = "exclusionRangeIncrease";
+	public static final String EX_MIN_SEQUENCE = "exclusionMinSequence";
+	public static final String EX_MAX_SEQUENCE = "exclusionMaxSequence";
+	
+	public static final String FR_ANAL_COUNT = "frequentAnalysisCount";
+	public static final String FR_MIN_RANGE = "frequentMinRange";
+	public static final String FR_MAX_RANGE = "frequentMaxRange";
+	public static final String FR_RANGE_INC = "frequentRangeIncrease";
+	public static final String FR_MIN_SEQUENCE = "frequentMinSequence";
+	public static final String FR_MAX_SEQUENCE = "frequentMaxSequence";
+}
---
+++ src/main/java/com/rs/lottoweb/rest/LottoController.java
@@ -0,0 +1,35 @@
+package com.rs.lottoweb.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rs.lottoweb.domain.LottoNumbers;
+import com.rs.lottoweb.service.LottoService;
+
+
+@RestController
+@RequestMapping("/anal")
+public class LottoController {
+	
+	@Autowired
+	LottoService lottoService;
+
+	@RequestMapping(value="/{round}", method=RequestMethod.GET)
+	public LottoNumbers recommend(@PathVariable(value="round") int round) throws Exception{
+		
+		List<Integer> nums = lottoService.getAnalysedFrequentNumbers(round);
+		
+		if(nums == null || nums.size() < 1)
+			throw new Exception("not analysed");
+		LottoNumbers lottoNums = new LottoNumbers();
+		lottoNums.setRound(round);
+		lottoNums.setNums(nums);
+		
+		return lottoNums;
+	}
+}
